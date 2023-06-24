e579a939f1da7e302b66367eb7cfe0dfe4ac6e8e
Leo Tumwattana <leo.tumwattana@gmail.com>
2023-06-24 13:06:40 +0800

    Initial Commit

---
+++ UITextItemTagAttributeName-Interaction/UITextItemTagAttributeName_InteractionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITextItemTagAttributeName_InteractionApp.swift
+//  UITextItemTagAttributeName-Interaction
+//
+//  Created by Leo Tumwattana on 24/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct UITextItemTagAttributeName_InteractionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
